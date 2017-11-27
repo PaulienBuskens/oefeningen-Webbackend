f8e3b37cd6ccc01a66ab6f726a0470ffc0c69852
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2017-11-27 17:14:29 +0100

    kl

--- opdrachten/oplossingen/27-crud-query/27-crud-query-deel2.php
+++ opdrachten/oplossingen/27-crud-query/27-crud-query-deel2.php
@@ -1,31 +1,55 @@
 <?php
 
     $message = '';
+    $geselecteerdeBrouwer = '';
 
     try{
     
     $db = new pdo('mysql:host=localhost;dbname=bieren', 'root','');
 
 
-        $sql = 'SELECT brnaam, brouwernr FROM brouwers';
+        $brouwerSql = 'SELECT brnaam, brouwernr FROM brouwers';
 
-        $result = $db->query($sql);
-
-        $statement = $db->prepare( $sql );
+        $brouwersStatement = $db->prepare( $brouwerSql );
         
-        $statement->execute( );
+        $brouwersStatement->execute( );
 
-        $brouwers	=array();
+        $brouwers  = array();
         
-            while ( $row = $result->fetch(PDO::FETCH_ASSOC)){
+            while ( $row = $brouwersStatement->fetch(PDO::FETCH_ASSOC)){
                     $brouwers[] 	=	$row;
             }
+
+            if( isset($_GET['brouwernr'])){
+                
+                $geselecteerdeBrouwer = $_GET['brouwernr'];
+                $bierenSql = 'SELECT bieren.naam
+                        FROM bieren
+                        WHERE bieren.brouwernr = :brouwernr';
+                
+                $bierenStatement = $db->prepare($bierenSql);
+                $bierenStatement->bindValue(' :brouwernr', $_GET['brouwernr']);
+
+            }else {
+                $bierenSql = 'SELECT bieren.naam
+                        FROM bieren';
+                
+                $bierenStatement = $db->prepare($bierenSql);
+            }
+
+            $bierenStatement->execute();
+
+            $bierenHeader   = array();
+            $bierenHeader[] = 'Aantal';
         
-            $columnNames	=	array();
-            $columnNames[]	=	'Biernummer';
-        
-            foreach( $brouwers[0] as $key => $bier ){
-                    $columnNames[  ]	=	$key;
+            for($columnNumber = 0; $columnNumber < $bierenStatement->columnCount( ); ++$columnNumber){
+                $bierenHeader[] = $bierenStatement->getColumnMeta($columnNumber)['name'];
+            }
+
+            $bieren = array();
+
+            while($row = $bierenStatement->fetch(PDO::FETCH_ASSOC)){
+                $bieren[ ] = $row['naam'];
             }
 
     }catch ( PDOException $e ){
@@ -42,9 +66,68 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Opdracht CRUD query</title>
        
+        <style>
+			.modal
+			{
+				padding			:	6px;
+				border-radius	:	3px;
+			}
+
+			.success
+			{
+				background-color:lightgreen;
+			}
+
+			.error
+			{
+				background-color:red;
+			}
+
+			.even
+			{
+				background-color:lightgrey;
+			}
+		</style>
 	</head>
 
     <body class="web-backend-opdracht">
 
-    </body>
+    <?php if ( $message ): ?>
+		<div class="modal <?= $message[ "type" ] ?>">
+			<?= $message['text'] ?>
+		</div>
+	<?php endif ?>
+
+	<form action="<?= $_SERVER['PHP_SELF'] ?>" method="GET">
+		
+		<select name="brouwernr">
+			<?php foreach ($brouwers as $key => $brouwer): ?>
+				<option value="<?= $brouwer['brouwernr'] ?>" <?= ( $geselecteerdeBrouwer === $brouwer['brouwernr'] ) ? 'selected' : '' ?>><?= $brouwer['brnaam'] ?></option>
+			<?php endforeach ?>
+		</select>
+		<input type="submit" value="Geef mij alle bieren van deze brouwerij">
+	</form>
+	
+    <table>
+
+    <thead>
+        <tr>
+            <?php foreach ($bierenHeader as $columnName): ?>
+                <th><?= $columnName ?></th>
+            <?php endforeach ?>
+        </tr>
+    </thead>
+
+    <tbody>
+    
+        <?php foreach ($bieren as $key => $biernaam): ?>
+            <tr class="<?= ( ( $key + 1) %2 == 0 ) ? 'even' : '' ?>">
+                <td><?= ( $key + 1 ) ?></td>
+                <td><?= $biernaam ?></td>
+            </tr>
+        <?php endforeach ?>
+
+    </tbody>
+
+</table>
 </html>
