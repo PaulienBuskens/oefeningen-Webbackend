8e6bbb86c88c6a8b8e713aed5309798b9c4eb248
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2018-01-06 17:34:07 +0100

    kl

---
+++ opdrachten/oplossingen/42-mod-rewrite-single-point-of-entry/classes/delete.php
@@ -0,0 +1,9 @@
+<?php 
+
+    class Delete{
+        public function __construct($table, $id){
+            echo '<h1>Delete van ' . $table . '</h1>';
+        }
+    }
+
+?>
---
+++ opdrachten/oplossingen/42-mod-rewrite-single-point-of-entry/classes/insert.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Insert{
+        public function __construct($table){
+            echo '<h1>Insert in ' . $table . '</h1>';
+        }
+    }
+
+?>
